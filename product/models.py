91792535a88af63f0532284785c82a45d83f5b32
werdani <https://github.com/werdani/django-job-board.git>
2020-11-11 20:51:29 +0200

    create produt model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    PRDName = models.CharField(max_length=100)
+    #category = models.CharField(max_length=100)
+    PRDDesc = models.TextField(max_length=1000)
+    PRDPrice = models.DecimalField(max_digits=5,decimal_places=2)
+    PRDCost = models.DecimalField(max_digits=5,decimal_places=2)
+    PRDCreated = models.DateTimeField()
+
+
+
+    def __str__(self):
+         return self.PRDName
+ 
+
+
