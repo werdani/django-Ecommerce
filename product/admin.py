bcd8c56afce80177fb3946a446e972f6a48f97d2
werdani <https://github.com/werdani/django-job-board.git>
2020-11-11 22:52:44 +0200

    create model category

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin 
-from .models import Product , ProductImage
+from .models import Product , ProductImage,Category
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(Category)
--- product/models.py
+++ product/models.py
@@ -21,3 +21,20 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return str(self.PRDIproduct)
+
+class Category(models.Model):
+    CATname = models.CharField(max_length=50,verbose_name=_("category name"))
+    # self this is recersive relation ship . 
+    # to view filter category main only i'am used  limit_choices_to .
+    CATpearent = models.ForeignKey('self',limit_choices_to={'CATpearent__isnull':True},on_delete=models.CASCADE,blank=True, null=True)
+    CATdesc = models.TextField(max_length=1000,verbose_name=_("category descereption"))
+    CATimage = models.ImageField(upload_to='category/',verbose_name=_("category image"))
+    def __str__(self):
+        return str(self.CATname)
+
+
+
+
+
+
+
