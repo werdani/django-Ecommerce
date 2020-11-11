c06a74b1fe8d59100c8fccc20a3b1b57466edde9
werdani <https://github.com/werdani/django-job-board.git>
2020-11-11 23:49:08 +0200

    create 2 models product accessoris and alternative

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin 
-from .models import Product , ProductImage,Category
+from .models import Product , ProductImage,Category,Product_Alternative,Product_Accessoris
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(Category)
+admin.site.register(Product_Alternative)
+admin.site.register(Product_Accessoris)
--- product/models.py
+++ product/models.py
@@ -10,6 +10,9 @@ class Product(models.Model):
     PRDPrice = models.DecimalField(max_digits=5,decimal_places=2,verbose_name=_('product price'))
     PRDCost = models.DecimalField(max_digits=5,decimal_places=2,verbose_name=_('product cost'))
     PRDCreated = models.DateTimeField(verbose_name=_('created at'))
+    class Meta:
+        verbose_name = _("Product") # for single 
+        verbose_name_plural = _("Products") # for collection
 
     def __str__(self):
          return self.PRDName
@@ -18,6 +21,10 @@ class Product(models.Model):
 class ProductImage(models.Model):
     PRDIproduct = models.ForeignKey(Product,on_delete=models.CASCADE,verbose_name=_('product'))
     PRDIimag = models.ImageField(upload_to='product/' ,verbose_name=_("Image"))
+    class Meta:
+        verbose_name = _("ProductImage") # for single 
+        verbose_name_plural = _("ProductImages") # for collection
+
 
     def __str__(self):
         return str(self.PRDIproduct)
@@ -26,11 +33,45 @@ class Category(models.Model):
     CATname = models.CharField(max_length=50,verbose_name=_("category name"))
     # self this is recersive relation ship . 
     # to view filter category main only i'am used  limit_choices_to .
-    CATpearent = models.ForeignKey('self',limit_choices_to={'CATpearent__isnull':True},on_delete=models.CASCADE,blank=True, null=True)
+    CATpearent = models.ForeignKey('self',limit_choices_to={'CATpearent__isnull':True},verbose_name=_("main category"),on_delete=models.CASCADE,blank=True, null=True)
     CATdesc = models.TextField(max_length=1000,verbose_name=_("category descereption"))
     CATimage = models.ImageField(upload_to='category/',verbose_name=_("category image"))
+    class Meta:
+        verbose_name = _("Category") # for single 
+        verbose_name_plural = _("Categoryes") # for collection
+
     def __str__(self):
         return str(self.CATname)
+
+
+
+class Product_Alternative (models.Model):
+    PALNproduct = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='main_product',verbose_name=_("product"))
+    PALNAlternative = models.ManyToManyField(Product,related_name='alternative_products',verbose_name=_("Alternatve"))
+    
+
+    class Meta:
+        verbose_name = _("Product Alternative")
+        verbose_name_plural = _("Product Alternatives")
+
+    def __str__(self):
+        return str(self.PALNproduct)
+
+
+
+
+class Product_Accessoris (models.Model):
+    PACCproduct = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='mainACCESSORS_product',verbose_name=_("product"))
+    PACCAlternative = models.ManyToManyField(Product,related_name='alternativeACCESSORS_products',verbose_name=_("Accessoris"))
+    
+
+    class Meta:
+        verbose_name = _("Product Accessoris")
+        verbose_name_plural = _("Product Accessoris")
+
+    def __str__(self):
+        return str(self.PACCproduct)
+
 
 
 
