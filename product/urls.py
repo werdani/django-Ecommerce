a2e12a28e2eb6d519c0313a7bf797afdcc49c22d
werdani <https://github.com/werdani/django-job-board.git>
2020-11-12 23:31:46 +0200

    add url for all producte in product_detail

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+
+app_name='product'
 urlpatterns = [
     path('',views.product_list),
-    path('<int:id>/',views.product_detail),
+    path('<int:id>/',views.product_detail,name='product_detail'),
 
 ]
