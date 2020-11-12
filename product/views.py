85f49dd93ab82fd6639795e653bdf395f7b2f0bd
werdani <https://github.com/werdani/django-job-board.git>
2020-11-12 22:50:48 +0200

    add paginator in product_list

--- product/views.py
+++ product/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from . models import Product
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 def product_list(request):
     product_list = Product.objects.all()
+    
+    paginator = Paginator(product_list, 4) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    product_list = paginator.get_page(page_number)
 
     return render(request,'product/product_list.html',{'product_list':product_list}) 
 
