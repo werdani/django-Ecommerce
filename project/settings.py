e213a73a20213f10ea5a7a37faa1c42be611071d
werdani <https://github.com/werdani/django-job-board.git>
2020-11-12 18:24:24 +0200

    add static and css and images

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.parh.join(BASE_DIR,'static','site_static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
