bb92f8025b291e6b96ec2358cafd0ee1f554797c
werdani <https://github.com/werdani/django-job-board.git>
2020-11-14 23:33:15 +0200

    add get_object_or_404

--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 
 from . models import Product
 from django.core.paginator import Paginator
 # Create your views here.
@@ -14,6 +14,7 @@ def product_list(request):
 
 
 def product_detail(request,slug):
-    product_details = Product.objects.get(PRDSlug=slug)
+    #product_details = Product.objects.get(PRDSlug=slug)
+    product_details = get_object_or_404(Product,PRDSlug=slug)
 
     return render(request,'product/product_detail.html',{'product_details':product_details})
