406218775c198a92562e3d761b98a2283e922350
werdani <https://github.com/werdani/django-job-board.git>
2020-11-16 01:36:39 +0200

    create anew app accounts

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls' ,namespace='products'))
+    path('', include('product.urls' ,namespace='products')),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
